61ad327f9d1be9c11b15fb49c6b6dbfd919b00a8
owendswang <9076865+owendswang@users.noreply.github.com>
2022-11-25 19:41:46 +0800

    Update Load pictures on ODSW.user.js

--- Load pictures on ODSW.user.js
+++ Load pictures on ODSW.user.js
@@ -2,7 +2,7 @@
 // @name         Load pictures on ODSW
 // @name:zh-CN   加载ODSW网站上的图片
 // @namespace    https://www.owendswang.com/
-// @version      0.2
+// @version      0.3
 // @description  Load pictures from weibo.com without limitation on 'owendswang.com'.
 // @description:zh-CN  跳过微博图床外链限制，加载“owendswang.com”网站上的微博外链图片。
 // @icon         https://avatars.githubusercontent.com/u/9076865?s=40&v=4
@@ -39,6 +39,8 @@
                             el.src = URL.createObjectURL(response);
                         } else if (el.tagName === "DIV") {
                             el.style.backgroundImage = 'url(\'' + URL.createObjectURL(response) + '\')';
+                        } else if (el.tagName === "VIDEO") {
+                            el.setAttribute('poster', URL.createObjectURL(response))
                         }
                     }
                 }
